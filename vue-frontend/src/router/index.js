533d1390c6539ffb9c9c4c25d0558130fce1c692
Matthew The <matthew.the@tum.de>
2025-12-18 22:40:14 +0100

    Refactor AnalyticsView with routes

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import LandingPage from '@/components/LandingPage.vue'
 import AbundanceComponent from '@/components/AbundanceComponent.vue'
-import AnalyticsComponent from '@/components/AnalyticsComponent.vue'
+
 import PatientComponent from '@/components/PatientComponent.vue'
 import AdminToolsComponent from '@/components/AdminToolsComponent.vue'
 
@@ -12,6 +12,15 @@ import KinaseComponent from '@/components/KinaseComponent.vue'
 import PproteinComponent from '@/components/ProteinScores.vue'
 import EntityComponent from '@/components/entitymodelComponent.vue'
 import ZscoringComponent from '@/components/ZscoringComponent.vue'
+
+import AnalyticsComponent from '@/views/AnalyticsView.vue'
+import PCAComponent from '@/components/QCComponent.vue'
+import CorrelationComponent from '@/components/CorrelationComponent.vue'
+import DifferentialComponent from '@/components/DifferentialComponent.vue'
+import VennComponent from '@/components/VennComponent.vue'
+import HeatmapComponent from '@/components/HeatmapComponent.vue'
+import KinobeadsComponent from '@/components/DrugComponent.vue'
+import PTMNavigatorComponent from '@/components/PTMNavigatorComponent.vue'
 
 Vue.use(Router)
 
@@ -35,7 +44,21 @@ export default new Router({
           { path: '/zscores', component: ZscoringComponent }
         ]
     },
-    { path: '/analytics', component: AnalyticsComponent },
+    {
+      path: '/analytics',
+      component: AnalyticsComponent,
+      redirect: '/correlation',
+      children:
+        [
+          { path: '/correlation', name: 'correlation', component: CorrelationComponent },
+          { path: '/pca', name: 'pca', component: PCAComponent },
+          { path: '/differential', name: 'differential', component: DifferentialComponent },
+          { path: '/heatmap', name: 'heatmap', component: HeatmapComponent },
+          { path: '/venn', name: 'venn', component: VennComponent },
+          { path: '/kinobeads', name: 'kinobeads', component: KinobeadsComponent },
+          { path: '/ptmnavigator', name: 'ptmnavigator', component: PTMNavigatorComponent }
+        ]
+    },
     { path: '/patient', component: PatientComponent },
     { path: '/admin-tools', component: AdminToolsComponent }
   ]
---
+++ vue-frontend/src/views/AnalyticsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-main>
+    <v-tabs>
+      <v-tab to="/correlation" exact>Correlation</v-tab>
+      <v-tab to="/pca" exact>PCA</v-tab>
+      <v-tab to="/differential" exact>Differential</v-tab>
+      <v-tab to="/heatmap" exact>Heatmap</v-tab>
+      <v-tab to="/venn" exact>Venn</v-tab>
+      <v-tab to="/kinobeads" exact>Kinobeads</v-tab>
+      <v-tab to="/ptmnavigator" exact>PTM Navigator</v-tab>
+    </v-tabs>
+
+    <router-view />
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'AnalyticsComponent'
+}
+</script>
+
+<style>
+
+</style>
